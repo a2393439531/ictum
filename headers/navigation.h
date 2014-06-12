a98733a03c4bded5c2d56761161b0e5175cf246c
Teodoro Alcantara Aquino <teodoro641@hotmail.com>
2014-06-12 00:30:13 -0400

    Added deleteScreenWidgets functions to Navigation class
    I also deleted a bunch of unnecessary stuff, so the class look cleaner now

--- headers/navigation.h
+++ headers/navigation.h
@@ -2,18 +2,9 @@
 #define NAVIGATION_H
 
 #include <QWidget>
-#include <QStack>
 
 class QVBoxLayout;
 class QPushButton;
-class QGridLayout;
-class QTableView;
-class QListView;
-class QTextEdit;
-class QLineEdit;
-class QPushButton;
-class QString;
-//class QStack<QString>;
 
 class Navigation : public QWidget 
 {
@@ -25,31 +16,10 @@ class Navigation : public QWidget
 	private slots:
 		void showRolesInfo();
 		void showOUInfo();
-        //void oUEntities(const QModelIndex& index);
-		//void showOUEntitiesElementInfo(const QModelIndex& index);
 		void showDataEntityInfo();
-		//void dataEntities(const QModelIndex& index);
-		//void showDataEntitiesElementInfo(const QModelIndex& index);
-		//void showTechnologiesComponentsInfo();
-		//void technologiesComponentsEntities(const QModelIndex& index);
-		//void showTechnologiesComponentsEntitiesElementInfo(const QModelIndex& index);
-		void entitySearch(const QString& text);
-		void goBack();
 
     private:
-        QWidget* startScreen;
-		QWidget* screen;
 		QVBoxLayout* mainlayout;
-		QGridLayout* dataViewLayout;
-		QTableView* currentAnalyzedEntityTable;
-		QListView* releatedEntityInformationListView;
-		QListView* relatedEntitiesListView;
-		QTextEdit* elementDescriptionTextEdit;
-		QTextEdit* descriptionTextEdit;
-		QLineEdit* searchEdit;
-		QPushButton* back;
-	    //bool isFirstTime = true;
-		QString currentScreen;
-		QStack<QString> navigationController;
+        void deleteScreenWidgets();
 };
 #endif
--- sources/navigation.cpp
+++ sources/navigation.cpp
@@ -5,34 +5,12 @@
 #include "../headers/mainwindow.h"
 #include <QVBoxLayout>
 #include <QPushButton>
-#include <QTableView>
-#include <QGroupBox>
-#include <QGridLayout>
-#include <QtSql>
-#include <QListView>
 #include <QLayoutItem>
-#include <QTextEdit>
-#include <QLineEdit>
-#include <QLabel>
-#include <QStandardItemModel>
-#include <QStack>
 
 Navigation::Navigation(QWidget* parent) : QWidget(parent)
 {
 	
-	/*if (!isFirstTime){
-		QLayoutItem* item; 
-		QWidget* widget;
-		qDebug()<<"aqui afuera";
-		this deletes the all the widgets that were at the screen before placing the widgets definded here
-		while ((item = dataViewLayout->takeAt(0))){
-			qDebug()<<"entreaqui";
-			if ((widget = item->widget()) !=0 ){widget->hide(); delete widget;}
-			else {delete item;}
-		}
-	}*/
-	startScreen = new QWidget;
-	mainlayout = new QVBoxLayout(startScreen);
+	mainlayout = new QVBoxLayout();
 	
     QPushButton* roles = new QPushButton("Roles");
 	roles->setFlat(true);
@@ -40,12 +18,10 @@ Navigation::Navigation(QWidget* parent) : QWidget(parent)
 	OU->setFlat(true);
 	QPushButton* dataEntity = new QPushButton("Entidad de Datos");
 	dataEntity->setFlat(true);
-	//QPushButton* technologyComponets = new("Componentes Fisicos Tecnologicos");
 	
 	mainlayout->addWidget(roles);
 	mainlayout->addWidget(OU);
 	mainlayout->addWidget(dataEntity);
-	//mainlayout->addWidget(technologyComponets);
 
 	setLayout(mainlayout);
 
@@ -57,10 +33,9 @@ Navigation::Navigation(QWidget* parent) : QWidget(parent)
 			this, SLOT(showDataEntityInfo()));
 }
 
-
-void Navigation::showRolesInfo()
+void Navigation::deleteScreenWidgets()
 {
-	QLayoutItem* item; 
+    QLayoutItem* item; 
 	QWidget* widget;
 		/*this deletes the all the widgets that were at the screen before placing the widgets definded here*/
 	while ((item = mainlayout->takeAt(0))){
@@ -68,81 +43,32 @@ void Navigation::showRolesInfo()
 		else {delete item;}
 	}
 
-	delete startScreen;    //deletes previos widgets on the screen
 	delete mainlayout;
+}
 
+
+void Navigation::showRolesInfo()
+{
+	deleteScreenWidgets();
+    
     // sets RolesNavigation widget as the central widget of the MainWindow
     MainWindow::instance()->setCentralWidget(new RolesNavigation(MainWindow::instance()));
 }
 
 void Navigation::showOUInfo()
 {
-	QLayoutItem* item; 
-	QWidget* widget;
-
-
-	/*this deletes the all the widgets that were at the screen before placing the widgets definded here*/
-	while ((item = mainlayout->takeAt(0))){
-		if ((widget = item->widget()) !=0 ){widget->hide(); delete widget;}
-		else {delete item;}
-	}
-
-	delete startScreen;    //deletes previos widgets on the screen
-	delete mainlayout;
-
+	deleteScreenWidgets();
     // set OuNavigation as the central widget in the MainWindow
     MainWindow::instance()->setCentralWidget(new OuNavigation(MainWindow::instance()));
 }
 
 
-
-/*******************************************************************Data Entity***************************************************/
-//---------------------------------------------------------------------------------------------------------------------------------
 void Navigation::showDataEntityInfo()
 {
-	
-	QLayoutItem* item; 
-	QWidget* widget;
-
-		/*this deletes the all the widgets that were at the screen before placing the widgets definded here*/
-	while ((item = mainlayout->takeAt(0))){
-		if ((widget = item->widget()) !=0 ){widget->hide(); delete widget;}
-		else {delete item;}
-	}
-
-	delete startScreen;    //deletes previos widgets on the screen
-	delete mainlayout;
-
+	deleteScreenWidgets();
+    
     // sets DataEntityNavigation widget as the central of the mainwindow
     MainWindow::instance()->setCentralWidget(new DataEntityNavigation(MainWindow::instance()));
-	/***********************************************************************************************************/
-
 													   
 }
 
-void Navigation::entitySearch(const QString& text)
-{
-	/* search roles using the user input in the LineEdit*/
-	
-	QSqlQueryModel* searchRoles = new QSqlQueryModel;
-    /*******************************************Roles Scren***********************************************************************/
-	if (currentScreen == "rolesScreen"){
-		if (text != "")
-			searchRoles->setQuery(QString("SELECT rol_id, name FROM Roles WHERE name LIKE '%1%'").arg(text));
-		else
-			searchRoles->setQuery("SELECT rol_id, name FROM Roles");
-	}
-	/********************************************OranizationalUnits Screen*********************************************************/
-	/**************************************************************Data Entity ************************************************/
-		currentAnalyzedEntityTable->setModel(searchRoles);
-}
-
-
-void Navigation::goBack()
-{
-	if(navigationController.pop() == "startScreen"){
-		qDebug()<<"tamo en start";
-		Navigation();
-	}
-	
-}
